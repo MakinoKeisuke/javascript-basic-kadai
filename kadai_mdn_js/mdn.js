96af9d65fc3e94ec8fc98e08c6805291cbbb8327
makinokeisuke <makino.19820629@gmail.com>
2023-11-14 09:15:09 +0900

    セブンコミット

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+var nameDate = new Date([2022, 8, 22]);
+console.log(nameDate.getFullYear);
+
+var year = nameDate.getFullYear();
+var month = nameDate.getMonth() + 1;
+var day = nameDate.getDate();
+
+console.log(year);
+console.log(month);
+console.log(day);
